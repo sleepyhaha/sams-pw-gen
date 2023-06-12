967707ef2b8da3bc922763290d35e0ca6a0c5b88
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-06-12 13:00:39 +1000

    add inital generate password function

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,29 @@
 // Assignment code here
 
+//
+function generatePassword(length) {
+  const characters = "abcdefghijklmnopqrstuvwxyz";
+  const upperCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numbers = "012345679";
+  const specialCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+  let result = "";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; index++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
