ec07ee5afc616df39deaf3fe1b0e79c673c43029
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-06-15 18:27:34 +1000

    finalised working pw generator

--- Develop/script.js
+++ Develop/script.js
@@ -2,33 +2,61 @@
 
 //
 const characters = {
-  lowerCharacters: "abcdefghijklmnopqrstuvwxyz",
-  upperCharacters: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowerCase: "abcdefghijklmnopqrstuvwxyz",
+  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   numbers: "012345679",
-  specialCharacters: "!#$%&'()*+,-./:;<=>?@[]^_`{|}~",
+  special: "!#$%&'()*+,-./:;<=>?@[]^_`{|}~",
 };
 
-function generatePassword(length) {
-  let result = "";
-  const charactersLength = characters.length;
-  for (let i = 0; i < length; i++) {
-    result += Math.floor(Math.random() * characters);
-  }
-
-  return result;
-}
-
+let charFinal = "";
 // Get references to the #generate element
 const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+function promptUser() {
+  let passwordLength = window.prompt(
+    "Please select a password length between 8 and 128 characters"
+  );
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    confirm("Please enter a value between 8 and 128.");
+    return;
+  } else {
+    answer = passwordLength;
+  }
+
+  let confirmLower = window.confirm("Would you like to add lowercase letters?");
+  if (confirmLower === true) {
+    charFinal += characters["lowerCase"];
+  }
+  let confirmUpper = window.confirm("Would you like to add uppercase letters?");
+  if (confirmUpper === true) {
+    charFinal += characters["upperCase"];
+  }
+  let confirmNumber = window.confirm("Would you like to add numbers?");
+  if (confirmNumber === true) {
+    charFinal += characters["numbers"];
+  }
+  let confirmSpecial = window.confirm(
+    "Would you like to add special characters?"
+  );
+  if (confirmSpecial === true) {
+    charFinal += characters["special"];
+  }
+
+  return generatePassword(passwordLength);
+}
+
+function generatePassword(passwordLength) {
+  let result = "";
+  const charFinalLength = charFinal.length;
+  for (let i = 0; i < passwordLength; i++) {
+    result += charFinal.charAt(Math.floor(Math.random() * charFinalLength));
+  }
+
+  return result;
+}
 function writePassword() {
-  window.prompt("Please select a password length between 8 and 128 characters");
-  window.confirm("Would you like to add lowercase letters?");
-  window.confirm("Would you like to add uppercase letters?");
-  window.confirm("Would you like to add numbers?");
-  window.confirm("Would you like to add special characters?");
-  const password = generatePassword();
+  const password = promptUser();
   const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
